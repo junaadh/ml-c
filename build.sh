942b486a6507464efa4d295aaf0e7bb82c64b9b7
Moosa Junaadh <junaadh.02@gmail.com>
2023-12-30 03:33:42 +0500

    gates and improved xor

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 
 set -xe
 
-clang -Wall -Wextra -o double double.c
-clang -Wall -Wextra -o gates gates.c
+clang -Wall -Wextra -o double double.c -lm
+clang -Wall -Wextra -o gates gates.c -lm
+clang -Wall -Wextra -o xor xor.c -lm
