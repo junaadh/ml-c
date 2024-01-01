4b10fffd95a5105e124a5d4117420301902cb568
Moosa Junaadh <junaadh.02@gmail.com>
2024-01-02 01:25:13 +0500

    basic framework

--- src/ars.c
+++ src/ars.c
@@ -2,130 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+// sepcify that we want the ARS_INPLEMENTATION from ars.h
 #define ARS_IMPLEMENTATION
 #include "ars.h"
 
-typedef struct {
-  Mat a0, a1, a2;
-
-  Mat w1, b1;
-  Mat w2, b2;
-} Xor;
-
-Xor alloc(void) {
-  Xor m;
-
-  m.a0 = mat_alloc(1, 2);
-  m.w1 = mat_alloc(2, 2);
-  m.b1 = mat_alloc(1, 2);
-  m.a1 = mat_alloc(1, 2);
-
-  m.w2 = mat_alloc(2, 1);
-  m.b2 = mat_alloc(1, 1);
-  m.a2 = mat_alloc(1, 1);
-  return m;
-}
-
-void forward_xor(Xor m) {
-  mat_dot(m.a1, m.a0, m.w1);
-  mat_sum(m.a1, m.b1);
-  mat_sig(m.a1);
-
-  mat_dot(m.a2, m.a1, m.w2);
-  mat_sum(m.a2, m.b2);
-  mat_sig(m.a2);
-}
-
-float cost(Xor m, Mat ti, Mat to) {
-  ARS_ASSERT(ti.rows == to.rows);
-  ARS_ASSERT(to.cols == m.a2.cols);
-
-  size_t n = ti.rows;
-  float c = 0.f;
-  for (size_t i = 0; i < n; ++i) {
-    Mat x = mat_row(ti, i);
-    Mat y = mat_row(to, i);
-
-    mat_copy(m.a0, x);
-    forward_xor(m);
-
-    size_t q = to.cols;
-    for (size_t j = 0; j < q; ++j) {
-      float d = MAT_AT(m.a2, 0, j) - MAT_AT(y, 0, j);
-      c += d * d;
-    }
-  }
-
-  return c / n;
-}
-
-void finite_diff(Xor m, Xor g, float eps, Mat ti, Mat to) {
-  float saved;
-
-  float c = cost(m, ti, to);
-  for (size_t i = 0; i < m.w1.rows; ++i) {
-    for (size_t j = 0; j < m.w1.cols; ++j) {
-      saved = MAT_AT(m.w1, i, j);
-      MAT_AT(m.w1, i, j) += eps;
-      MAT_AT(g.w1, i, j) = (cost(m, ti, to) - c) / eps;
-      MAT_AT(m.w1, i, j) = saved;
-    }
-  }
-  for (size_t i = 0; i < m.b1.rows; ++i) {
-    for (size_t j = 0; j < m.b1.cols; ++j) {
-      saved = MAT_AT(m.b1, i, j);
-      MAT_AT(m.b1, i, j) += eps;
-      MAT_AT(g.b1, i, j) = (cost(m, ti, to) - c) / eps;
-      MAT_AT(m.b1, i, j) = saved;
-    }
-  }
-  for (size_t i = 0; i < m.w2.rows; ++i) {
-    for (size_t j = 0; j < m.w2.cols; ++j) {
-      saved = MAT_AT(m.w2, i, j);
-      MAT_AT(m.w2, i, j) += eps;
-      MAT_AT(g.w2, i, j) = (cost(m, ti, to) - c) / eps;
-      MAT_AT(m.w2, i, j) = saved;
-    }
-  }
-  for (size_t i = 0; i < m.b2.rows; ++i) {
-    for (size_t j = 0; j < m.b2.cols; ++j) {
-      saved = MAT_AT(m.b2, i, j);
-      MAT_AT(m.b2, i, j) += eps;
-      MAT_AT(g.b2, i, j) = (cost(m, ti, to) - c) / eps;
-      MAT_AT(m.b2, i, j) = saved;
-    }
-  }
-}
-
-void learn(Xor m, Xor g, float rate) {
-  for (size_t i = 0; i < m.w1.rows; ++i) {
-    for (size_t j = 0; j < m.w1.cols; ++j) {
-      MAT_AT(m.w1, i, j) -= rate * MAT_AT(g.w1, i, j);
-    }
-  }
-  for (size_t i = 0; i < m.b1.rows; ++i) {
-    for (size_t j = 0; j < m.b1.cols; ++j) {
-      MAT_AT(m.b1, i, j) -= rate * MAT_AT(g.b1, i, j);
-    }
-  }
-  for (size_t i = 0; i < m.w1.rows; ++i) {
-    for (size_t j = 0; j < m.w1.cols; ++j) {
-      MAT_AT(m.w2, i, j) -= rate * MAT_AT(g.w2, i, j);
-    }
-  }
-  for (size_t i = 0; i < m.b1.rows; ++i) {
-    for (size_t j = 0; j < m.b1.cols; ++j) {
-      MAT_AT(m.b2, i, j) -= rate * MAT_AT(g.b2, i, j);
-    }
-  }
-}
-
-void init_values(Xor m, float x1, float x2) {
-  MAT_AT(m.a0, 0, 0) = x1;
-  MAT_AT(m.a0, 0, 1) = x2;
-}
-
 // clang-format off
+// training data set
+// formatting turned off for legibility coz clangd does single line arrays
 float td[] = {
   0, 0, 0,
   0, 1, 1,
@@ -135,43 +18,66 @@ float td[] = {
 // clang-format on
 
 int main(void) {
+  // set the randomizing token
   srand(time(0));
 
-  size_t stride = 3;
-  size_t n = sizeof(td) / sizeof(td[0]) / 3;
-  Mat ti = {.rows = n, .cols = 2, .stride = stride, .es = td};
-  Mat to = {.rows = n, .cols = 1, .stride = stride, .es = td + 2};
-
-  // MAT_PRINT(ti);
-  // MAT_PRINT(to);
-  // return 0;
-
-  Xor m = alloc();
-  Xor g = alloc();
-
-  mat_rand(m.w1, 0, 1);
-  mat_rand(m.b1, 0, 1);
-  mat_rand(m.w2, 0, 1);
-  mat_rand(m.b2, 0, 1);
-
+  // set epselon and rate for learning
   float eps = 1e-1;
   float rate = 1e-1;
 
-  printf("cost = %f\n", cost(m, ti, to));
-  for (size_t i = 0; i < 100 * 1000; ++i) {
-    finite_diff(m, g, eps, ti, to);
-    learn(m, g, rate);
-    printf("%zu: cost = %f\n", i, cost(m, ti, to));
+  // the stride at which you want to form a  submatrix
+  size_t stride = 3;
+  // the number of rows in the training data set
+  size_t n = sizeof(td) / sizeof(td[0]) / stride;
+
+  // sub matrix of only training data inputs
+  Mat ti = {.rows = n, .cols = 2, .stride = stride, .es = td};
+  // sub matrix of training data output
+  Mat to = {.rows = n, .cols = 1, .stride = stride, .es = td + 2};
+
+  // architecture of the neural network
+  // arch[0] represents input params according to training data
+  // arch[ARRAY_LEN(arch) - 1] represents the output according to the training
+  // data
+  size_t arch[] = {
+      2,
+      2,
+      10,
+      1,
+  };
+
+  // base neural network
+  Net net = net_alloc(arch, ARRAY_LEN(arch));
+  // gradient neural network
+  Net grad = net_alloc(arch, ARRAY_LEN(arch));
+
+  // initialize random values in the network
+  net_rand(net, 0, 1);
+
+  // printing the cost as the network learns
+  printf("initial cost = %f\n", net_cost(net, ti, to));
+  for (size_t i = 0; i < 1000 * 1000; ++i) {
+    net_diff(net, grad, eps, ti, to);
+    net_learn(net, grad, rate);
+// make if 0 if want to disable tracing
+#if 1
+    printf("%zu: cost = %f\n", i, net_cost(net, ti, to));
+#endif
   }
 
+// make 0 if dont wanna see the test case
 #if 1
-  printf("------------------------------------------\n");
+  printf("\n------------------------------------------\n");
 
   for (size_t i = 0; i < 2; ++i) {
     for (size_t j = 0; j < 2; ++j) {
-      init_values(m, i, j);
-      forward_xor(m);
-      printf("%zu ^ %zu = %f\n", i, j, *m.a2.es);
+      // set the inputs
+      MAT_AT(NET_INPUT(net), 0, 0) = i;
+      MAT_AT(NET_INPUT(net), 0, 1) = j;
+      // use the model to get the output
+      net_forward(net);
+      // print the output
+      printf("%zu ^ %zu = %f\n", i, j, MAT_AT(NET_OUTPUT(net), 0, 0));
     }
   }
 #endif
--- src/ars.h
+++ src/ars.h
@@ -18,19 +18,31 @@
 #define ARS_ASSERT assert
 #endif // ARS_ASSERT
 
+//
+// macros
+//
+#define ARRAY_LEN(xs) sizeof((xs)) / sizeof((xs)[0])
+// matrix value at MAT_AT(source, rows, cols)
+#define MAT_AT(m, i, j) (m).es[(i) * (m).stride + (j)]
+// takes in a matrix and prints formated print statement
+#define MAT_PRINT(m) mat_print(m, #m, 0)
+// takes in a network and prints foramted print statement
+#define NET_PRINT(net) net_print(net, #net)
+
 // lightweight structure of matrix
 typedef struct {
   // number of rows in matrix
   size_t rows;
   // number of cols in matrix
   size_t cols;
+  // the strides to reach next row
   size_t stride;
   // pointer to the start of matrix
   float *es;
 } Mat;
 
 //
-// function declarations
+// matrix function declarations
 //
 
 float rand_float(void);
@@ -42,20 +54,40 @@ Mat mat_row(Mat m, size_t row);
 void mat_copy(Mat dest, Mat src);
 void mat_dot(Mat dest, Mat a, Mat b);
 void mat_sum(Mat dest, Mat a);
-void mat_print(Mat m, const char *name);
+void mat_print(Mat m, const char *name, size_t padding);
 void mat_fill(Mat m, float x);
 void mat_sig(Mat m);
 
-// matrix value at MAT_AT(source, rows, cols)
-#define MAT_AT(m, i, j) (m).es[(i) * (m).stride + (j)]
-// takes in a matrix and prints formated print statement
-#define MAT_PRINT(m) mat_print(m, #m)
+// structure of the neural network
+typedef struct {
+  size_t count;
+  Mat *ws;
+  Mat *bs;
+  Mat *as; // the amount of activations is count++
+} Net;
+
+#define NET_INPUT(net) (net).as[0]
+#define NET_OUTPUT(net) (net).as[(net).count]
+
+//
+// neural network function declarations
+//
+Net net_alloc(size_t *arch, size_t arch_count);
+void net_print(Net net, const char *name);
+void net_rand(Net net, float low, float high);
+void net_forward(Net net);
+float net_cost(Net net, Mat ti, Mat to);
+void net_diff(Net net, Net grad, float eps, Mat ti, Mat to);
+void net_learn(Net net, Net grad, float rate);
 
 #endif // ARS_H_
 
 // following logic only avaliable if ARS_IMPLEMENTATION defined
 #ifdef ARS_IMPLEMENTATION
 
+//
+// matrix
+//
 // returns a randomized float from range 0-1
 float rand_float(void) { return (float)rand() / RAND_MAX; }
 
@@ -134,15 +166,16 @@ void mat_sum(Mat dest, Mat a) {
 }
 
 // prints the matrix
-void mat_print(Mat m, const char *name) {
-  printf("%s = [\n", name);
+void mat_print(Mat m, const char *name, size_t padding) {
+  printf("%*s%s = [\n", (int)padding, "", name);
   for (size_t i = 0; i < m.rows; ++i) {
+    printf("%*s  ", (int)padding, "");
     for (size_t j = 0; j < m.cols; ++j) {
-      printf("    %f", MAT_AT(m, i, j));
+      printf("%f  ", MAT_AT(m, i, j));
     }
     printf("\n");
   }
-  printf("]\n");
+  printf("%*s]\n", (int)padding, "");
 }
 
 // fills matrix with float x
@@ -165,4 +198,115 @@ void mat_sig(Mat m) {
   }
 }
 
+//
+// Neural Netwoik
+//
+//
+Net net_alloc(size_t *arch, size_t arch_count) {
+  ARS_ASSERT(arch_count > 0);
+
+  Net net;
+  net.count = arch_count - 1;
+
+  net.ws = ARS_MALLOC(sizeof(*net.ws) * net.count);
+  ARS_ASSERT(net.ws != NULL);
+  net.bs = ARS_MALLOC(sizeof(*net.bs) * net.count);
+  ARS_ASSERT(net.bs != NULL);
+  net.as = ARS_MALLOC(sizeof(*net.as) * (net.count + 1));
+  ARS_ASSERT(net.as != NULL);
+
+  net.as[0] = mat_alloc(1, arch[0]);
+  for (size_t i = 1; i < arch_count; ++i) {
+    net.ws[i - 1] = mat_alloc(net.as[i - 1].cols, arch[i]);
+    net.bs[i - 1] = mat_alloc(1, arch[i]);
+    net.as[i] = mat_alloc(1, arch[i]);
+  }
+  return net;
+}
+
+void net_print(Net net, const char *name) {
+  char buf[256];
+  printf("%s = [\n", name);
+  for (size_t i = 0; i < net.count; ++i) {
+    snprintf(buf, sizeof(buf), "ws%zu", i);
+    mat_print(net.ws[i], buf, 4);
+    snprintf(buf, sizeof(buf), "bs%zu", i);
+    mat_print(net.bs[i], buf, 4);
+  }
+  printf("]\n");
+}
+
+void net_rand(Net net, float low, float high) {
+  for (size_t i = 0; i < net.count; ++i) {
+    mat_rand(net.ws[i], low, high);
+    mat_rand(net.bs[i], low, high);
+  }
+}
+
+void net_forward(Net net) {
+  for (size_t i = 0; i < net.count; ++i) {
+    mat_dot(net.as[i + 1], net.as[i], net.ws[i]);
+    mat_sum(net.as[i + 1], net.bs[i]);
+    mat_sig(net.as[i + 1]);
+  }
+}
+
+float net_cost(Net net, Mat ti, Mat to) {
+  ARS_ASSERT(ti.rows == to.rows);
+  ARS_ASSERT(to.cols == NET_OUTPUT(net).cols);
+  size_t n = ti.rows;
+
+  float c = 0;
+  for (size_t i = 0; i < n; ++i) {
+    Mat x = mat_row(ti, i);
+    Mat y = mat_row(to, i);
+
+    mat_copy(NET_INPUT(net), x);
+    net_forward(net);
+    size_t q = to.cols;
+    for (size_t j = 0; j < q; ++j) {
+      float d = MAT_AT(NET_OUTPUT(net), 0, j) - MAT_AT(y, 0, j);
+      c += d * d;
+    }
+  }
+  return c / n;
+}
+
+void net_diff(Net net, Net grad, float eps, Mat ti, Mat to) {
+  float saved;
+  float c = net_cost(net, ti, to);
+  for (size_t i = 0; i < net.count; ++i) {
+    for (size_t j = 0; j < net.ws[i].rows; ++j) {
+      for (size_t k = 0; k < net.ws[i].cols; ++k) {
+        saved = MAT_AT(net.ws[i], j, k);
+        MAT_AT(net.ws[i], j, k) += eps;
+        MAT_AT(grad.ws[i], j, k) = (net_cost(net, ti, to) - c) / eps;
+        MAT_AT(net.ws[i], j, k) = saved;
+      }
+    }
+    for (size_t j = 0; j < net.bs[i].rows; ++j) {
+      for (size_t k = 0; k < net.bs[i].cols; ++k) {
+        saved = MAT_AT(net.bs[i], j, k);
+        MAT_AT(net.bs[i], j, k) += eps;
+        MAT_AT(grad.bs[i], j, k) = (net_cost(net, ti, to) - c) / eps;
+        MAT_AT(net.bs[i], j, k) = saved;
+      }
+    }
+  }
+}
+
+void net_learn(Net net, Net grad, float rate) {
+  for (size_t i = 0; i < net.count; ++i) {
+    for (size_t j = 0; j < net.ws[i].rows; ++j) {
+      for (size_t k = 0; k < net.ws[i].cols; ++k) {
+        MAT_AT(net.ws[i], j, k) -= rate * MAT_AT(grad.ws[i], j, k);
+      }
+    }
+    for (size_t j = 0; j < net.bs[i].rows; ++j) {
+      for (size_t k = 0; k < net.bs[i].cols; ++k) {
+        MAT_AT(net.bs[i], j, k) -= rate * MAT_AT(grad.bs[i], j, k);
+      }
+    }
+  }
+}
 #endif // ARS_IMPLEMENTATION
