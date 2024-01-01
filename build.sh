791b06356797cb285008c05cdb611f05617ba9ed
Moosa Junaadh <junaadh.02@gmail.com>
2024-01-02 02:17:58 +0500

    moved old versions to src/prev

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@
 
 set -xe
 
-# clang -Wall -Wextra -o double double.c -lm
-# clang -Wall -Wextra -o gates gates.c -lm
-# clang -Wall -Wextra -o xor xor.c -lm
+mkdir -p build/prev
 
-mkdir -p build
+clang -Wall -Wextra -o build/prev/double src/prev/double.c -lm
+clang -Wall -Wextra -o build/prev/gates src/prev/gates.c -lm
+clang -Wall -Wextra -o build/prev/xor src/prev/xor.c -lm
+
 clang -Wall -Wextra -o build/ars src/ars.c -lm
