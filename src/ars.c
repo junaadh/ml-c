0251f1f7df4966933ee8d37384c9d910d42838ac
Moosa Junaadh <junaadh.02@gmail.com>
2024-01-02 02:13:57 +0500

    changed to allow for multiple gates

--- src/ars.c
+++ src/ars.c
@@ -6,20 +6,72 @@
 #define ARS_IMPLEMENTATION
 #include "ars.h"
 
+typedef struct {
+  size_t rows;
+  size_t cols;
+  float data[];
+} train_set_t;
+
 // clang-format off
 // training data set
 // formatting turned off for legibility coz clangd does single line arrays
-float td[] = {
+train_set_t td_or = {
+  .rows = 4, 
+  .cols = 3, 
+  .data = {
+  0, 0, 0,
+  0, 1, 1,
+  1, 0, 1,
+  1, 1, 1,
+  }
+};
+train_set_t td_and = {
+  .rows = 4, 
+  .cols = 3, 
+  .data = {
+  0, 0, 0,
+  0, 1, 0,
+  1, 0, 0,
+  1, 1, 1,
+  }
+};
+train_set_t td_nand = {
+  .rows = 4, 
+  .cols = 3, 
+  .data = {
+  0, 0, 1,
+  0, 1, 1,
+  1, 0, 1,
+  1, 1, 0,
+  }
+};
+train_set_t td_xor = {
+  .rows = 4, 
+  .cols = 3, 
+  .data = {
   0, 0, 0,
   0, 1, 1,
   1, 0, 1,
   1, 1, 0,
+  }
+};
+train_set_t td_nor = {
+  .rows = 4, 
+  .cols = 3, 
+  .data = {
+  0, 0, 1,
+  0, 1, 0,
+  1, 0, 0,
+  1, 1, 0,
+  }
 };
 // clang-format on
 
 int main(void) {
   // set the randomizing token
   srand(time(0));
+
+  train_set_t *td = &td_nand;
 
   // set epselon and rate for learning
   float eps = 1e-1;
@@ -28,12 +80,11 @@ int main(void) {
   // the stride at which you want to form a  submatrix
   size_t stride = 3;
   // the number of rows in the training data set
-  size_t n = sizeof(td) / sizeof(td[0]) / stride;
 
   // sub matrix of only training data inputs
-  Mat ti = {.rows = n, .cols = 2, .stride = stride, .es = td};
+  Mat ti = {.rows = td->rows, .cols = 2, .stride = stride, .es = td->data};
   // sub matrix of training data output
-  Mat to = {.rows = n, .cols = 1, .stride = stride, .es = td + 2};
+  Mat to = {.rows = td->rows, .cols = 1, .stride = stride, .es = td->data + 2};
 
   // architecture of the neural network
   // arch[0] represents input params according to training data
@@ -55,8 +106,8 @@ int main(void) {
   net_rand(net, 0, 1);
 
   // printing the cost as the network learns
-  printf("initial cost = %f\n", net_cost(net, ti, to));
-  for (size_t i = 0; i < 1000 * 1000; ++i) {
+  printf("\ninitial cost = %f\n", net_cost(net, ti, to));
+  for (size_t i = 0; i < 100 * 1000; ++i) {
     net_diff(net, grad, eps, ti, to);
     net_learn(net, grad, rate);
 // make if 0 if want to disable tracing
