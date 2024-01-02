38c421d6acba45ba75664bc9205e046b5ae75f43
Moosa Junaadh <junaadh.02@gmail.com>
2024-01-03 02:54:56 +0500

    updated docs

--- src/prev/double.c
+++ src/prev/double.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -8,11 +9,12 @@ float train[][2] = {{0, 0}, {1, 2}, {2, 4}, {3, 6}, {4, 8}};
 
 float rand_float(void) { return (float)rand() / (float)RAND_MAX; }
 
-float cost(float w, float b) {
+// differential old one
+float cost(float w) {
   float result = 0.0f;
   for (size_t i = 0; i < train_count; ++i) {
     float x = train[i][0];
-    float y = x * w + b;
+    float y = x * w;
     float d = y - train[i][1];
     result += d * d;
   }
@@ -20,21 +22,35 @@ float cost(float w, float b) {
   return result;
 }
 
-int main() {
-  srand(time(0));
-  float w = rand_float() * 10.0f;
-  float b = rand_float() * 5.0f;
-  float eps = 1e-3;
-  float rate = 1e-3;
-  printf("og cost: %f;  og w = %f, og b = %f\n", cost(w, b), w, b);
+// derivative new one
+float dcost(float w) {
+  float result = 0.0f;
+  for (size_t i = 0; i < train_count; ++i) {
+    float x = train[i][0];
+    float y = train[i][1];
+    result += 2 * (x * w - y) * x;
+  }
+  result /= train_count;
+  return result;
+}
 
-  for (size_t i = 0; i < 1000; ++i) {
-    float c = cost(w, b);
-    float dw = (cost(w + eps, b) - c) / eps;
-    float db = (cost(w, b + eps) - c) / eps;
+int main() {
+  // srand(time(0));
+  srand(69);
+  float w = rand_float() * 10.0f;
+  float rate = 1e-1;
+  printf("og cost: %f;  og w = %f\n", dcost(w), w);
+
+  for (size_t i = 0; i < 50; ++i) {
+#if 0     
+    float eps = 1e-1;
+    float c = cost(w);
+    float dw = (cost(w + eps) - c) / eps;
+#else
+    float dw = dcost(w);
+#endif
     w -= rate * dw;
-    b -= rate * db;
-    printf("cost: %f; w = %f, b = %f\n", cost(w, b), w, b);
+    printf("%zu: cost: %f; w = %f\n", i, cost(w), w);
   }
 
   printf("--------------------------\n");
@@ -44,7 +60,11 @@ int main() {
   // w needs to be 2
   //
 
-  printf("w = %f, b = %f\n", w, b);
+  printf("w = %f\n", w);
+
+  for (size_t i = 1; i <= 10; i += 2) {
+    printf("%zu > %f\n", i, i * w);
+  }
 
   return 0;
 }
