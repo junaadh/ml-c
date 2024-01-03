89e0ba437499ed606f5dc9072efbd2c81f745bf5
Moosa Junaadh <junaadh.02@gmail.com>
2024-01-03 21:30:41 +0500

    edit mistake in doc

--- build.sh
+++ build.sh
@@ -8,12 +8,10 @@ if [ $1 == "$doc" ]; then
 
   pdflatex -halt-on-error -output-directory doc src/doc/grad.tex
 else
-
-mkdir -p build/prev
-
-clang -Wall -Wextra -o build/prev/double src/prev/double.c -lm
-clang -Wall -Wextra -o build/prev/gates src/prev/gates.c -lm
-clang -Wall -Wextra -o build/prev/xor src/prev/xor.c -lm
-clang -Wall -Wextra -o build/gates src/gates.c -lm
+  mkdir -p build/prev
   
+  clang -Wall -Wextra -o build/prev/double src/prev/double.c -lm
+  clang -Wall -Wextra -o build/prev/gates src/prev/gates.c -lm
+  clang -Wall -Wextra -o build/prev/xor src/prev/xor.c -lm
+  clang -Wall -Wextra -o build/gates src/gates.c -lm
 fi
