1353bf7a66bf45227a5b2fe119dc3cf96a22c82f
Moosa Junaadh <junaadh.02@gmail.com>
2024-01-04 02:30:12 +0500

    added derivative cost calculation for prev/gates.c

--- src/prev/gates.c
+++ src/prev/gates.c
@@ -28,7 +28,7 @@ train_set_t nand[] = {
 };
 // clang-format on
 
-train_set_t *train = nand;
+train_set_t *train = and;
 
 size_t train_count = 4;
 // #define train_count (sizeof(train) / sizeof(train[0]))
@@ -48,6 +48,27 @@ float cost(float w1, float w2, float b) {
   return result;
 }
 
+void dcost(float w1, float w2, float b, float *dw1, float *dw2, float *db) {
+  *dw1 = 0;
+  *dw2 = 0;
+  *db = 0;
+
+  size_t n = train_count;
+  for (size_t i = 0; i < n; ++i) {
+    float xi = train[i][0];
+    float yi = train[i][1];
+    float zi = train[i][2];
+    float ai = sigmoidf(xi * w1 + yi * w2 + b);
+    float di = 2 * (ai - zi) * ai * (1 - ai);
+    *dw1 += di * xi;
+    *dw2 += di * yi;
+    *db += di;
+  }
+  *dw1 /= n;
+  *dw2 /= n;
+  *db /= n;
+}
+
 float rand_float(void) { return (float)rand() / (float)RAND_MAX; }
 
 int main() {
@@ -56,17 +77,22 @@ int main() {
   float w2 = rand_float();
   float b = rand_float();
 
-  float eps = 1e-1;
   float rate = 1e-1;
 
   // printf("w1 = %f, w2 = %f, b = %f, c = %f\n", w1, w2, b, cost(w1, w2, b));
-  for (size_t i = 0; i < 100 * 1000; ++i) {
+  for (size_t i = 0; i < 10000; ++i) {
     float c = cost(w1, w2, b);
-    // printf("w1 = %f, w2 = %f, b = %f c = %f\n", w1, w2, b, c);
+    printf("c = %f, w1 = %f, w2 = %f, b = %f\n", c, w1, w2, b);
     // printf("%f\n", c);
-    float dw1 = (cost(w1 + eps, w2, b) - c) / eps;
-    float dw2 = (cost(w1, w2 + eps, b) - c) / eps;
-    float db = (cost(w1, w2, b + eps) - c) / eps;
+    float dw1, dw2, db;
+#if 1
+    float eps = 1e-1;
+    dw1 = (cost(w1 + eps, w2, b) - c) / eps;
+    dw2 = (cost(w1, w2 + eps, b) - c) / eps;
+    db = (cost(w1, w2, b + eps) - c) / eps;
+#else
+    dcost(w1, w2, b, &dw1, &dw2, &db);
+#endif
     w1 -= rate * dw1;
     w2 -= rate * dw2;
     b -= rate * db;
@@ -75,7 +101,7 @@ int main() {
 
   for (size_t i = 0; i < 2; ++i) {
     for (size_t j = 0; j < 2; ++j) {
-      printf("%zu | %zu = %f\n", i, j, sigmoidf(i * w1 + j * w2 + b));
+      printf("%zu & %zu = %f\n", i, j, sigmoidf(i * w1 + j * w2 + b));
     }
   }
   return 0;
